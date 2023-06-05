ff018cd48ee3657f52ff8012dfc62b9bc0898c51
Anastasia <anastasiakosovets@gmail.com>
2023-06-05 22:09:51 +0200

    feat: create index.js and run server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5500;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('Servidor levantado en el puerto:' + PORT);
+})
