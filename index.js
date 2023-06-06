71939addaadb032729c064d4e893fe6774235a73
Anastasia <anastasiakosovets@gmail.com>
2023-06-06 19:55:20 +0200

    fix: resolve error of the port, delete app.listen because it was repeated

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
-// const db = require('./db');
+const db = require('./db');
 const app = express();
-const PORT = 9500;
+const PORT = 9000;
 app.use(express.json());
 
 app.get('/needToSleep', (req, res) => {
     return res.send('needToSleepy');
 })
-app.listen(PORT, () => {
-    console.log('Servidor levantado en el puerto:' + PORT);
-})
-
-// db.then(() => {
-//     app.listen(PORT, () => {
-//         console.log('Server is running on port:' + PORT);
-//     })
-// }).catch((error) => {
-//     console.error('Error starting server', error.message);
+// app.listen(PORT, () => {
+//     console.log('Servidor levantado en el puerto:' + PORT);
 // })
+
+db.then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port:' + PORT);
+    })
+}).catch((error) => {
+    console.error('Error starting server', error.message);
+})
