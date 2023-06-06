b76fed39e51f930e903c2ade236a27da09f1eb73
Anastasia <anastasiakosovets@gmail.com>
2023-06-06 20:03:45 +0200

    feat: create router file and modificate index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const db = require('./db');
+const router = require("./router");
 const app = express();
 const PORT = 9000;
-app.use(express.json());
 
-app.get('/needToSleep', (req, res) => {
-    return res.send('needToSleepy');
-})
-// app.listen(PORT, () => {
-//     console.log('Servidor levantado en el puerto:' + PORT);
+app.use(express.json());
+app.use(router);
+
+// app.get('/needToSleep', (req, res) => {
+//     return res.send('needToSleepy');
 // })
+
 
 db.then(() => {
     app.listen(PORT, () => {
