d1789e062ee7d96501462c33e3e76ce2d34b9b66
Anastasia <anastasiakosovets@gmail.com>
2023-06-06 22:17:34 +0200

    feat: create register of newUser and roleSeed, checked - it works

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,18 @@ authController.register = async (req, res) => {
         if ( req.body.password.length < 4 ) {
             return res.send('Password must be longer than 4 characters');
         }
-        
+        const newUser = await User.create ({
+            email: req.body.email,
+            password: req.body.password,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            document: req.body.document,
+            address: req.body.address,
+            telefonNumber: req.body.telefonNumber,
+            collegialNumber: req.body.collegialNumber,
+            role_id: 1
+        });
+        return res.send(newUser);
     } catch (error) {
         return res.send('Something went wrong creating user' + error.message);
     }
