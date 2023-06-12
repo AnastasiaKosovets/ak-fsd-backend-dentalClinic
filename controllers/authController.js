7d1d7206e32075438a8a41868bcdb19a59543bbd
Anastasia <anastasiakosovets@gmail.com>
2023-06-12 19:55:47 +0200

    fix: delete validations with Regex

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ authController.register = async (req, res) => {
                 console.log('Something went wrong with your register')
             
         }
-        
+        console.log("hola");
         // Part where encrypc password
         const newPassword = bcrypt.hashSync(req.body.password, 8);
         const newUser = await User.create ({
@@ -37,18 +37,34 @@ authController.register = async (req, res) => {
             address: req.body.address,
             telefonNumber: req.body.telefonNumber,
             collegialNumber: req.body.collegialNumber,
-            role_id: 1
+            role_id: 2
         });
+        
+    // Esta expresión regular garantiza que la contraseña cumpla con los siguientes requisitos:
+    // Al menos una letra mayúscula.
+    // Al menos una letra minúscula.
+    // Al menos un número.
+    // Al menos un carácter especial (@, $, !, %, *, ?, &).
+    // Longitud entre 8 y 14 caracteres. 
 
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        const isValidEmail = emailRegex.test(email);
+    //  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/;
+    //  const isValidPassword = passwordRegex.test(password)
+    //  if (!isValidPassword) {
+    //      return res.json({
+    //         success: true,
+    //         message: "Not a valid password"
+    //      })
+    //  }
+    
+    //     // const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        // const isValidEmail = emailRegex.test(email);
 
-        if (!isValidEmail) {
-            return res.json({
-                success: true,
-                message: "Not a valid email"
-            })
-        }
+        // if (!isValidEmail) {
+        //     return res.json({
+        //         success: true,
+        //         message: "Not a valid email"
+        //     })
+        // }
 
         return res.send(newUser);
     } catch (error) {
